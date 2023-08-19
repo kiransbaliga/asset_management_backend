3986c50d4ce925a975c9f0b09194790511b9a777
javedhamdan107 <javedhamdan@cet.ac.in>
2023-08-19 12:22:07 +0530

    Category Created

---
+++ src/dto/create-category.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+class CreateCategoryDto {
+    @IsNotEmpty()
+    @IsString()
+    name:string;
+    
+}
+
+export default CreateCategoryDto;
